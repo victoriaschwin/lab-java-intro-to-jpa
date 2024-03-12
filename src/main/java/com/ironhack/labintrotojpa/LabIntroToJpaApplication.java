2cb26f9d3ed1fcc78c5544e1fa1b0825f3b6a357
victoriaschwin <vicky.schwint95@gmail.com>
2024-03-12 21:27:06 +0100

    maven and spring boot

---
+++ src/main/java/com/ironhack/labintrotojpa/LabIntroToJpaApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.labintrotojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabIntroToJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabIntroToJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironhack/labintrotojpa/LabIntroToJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.labintrotojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabIntroToJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
